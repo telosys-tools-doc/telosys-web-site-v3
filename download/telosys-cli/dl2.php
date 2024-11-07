2e480b3d6ad61aca2cfb42a7a03f9b522dcdda3b
l-gu <laurent@telosys.org>
2024-11-07 23:58:21 +0100

    new 'index.html' + 'dl2.php'

---
+++ download/telosys-cli/dl2.php
@@ -0,0 +1,56 @@
+<?php
+function increment() {
+	$d = date("Y-m");
+	$f = "count-" . $d . ".txt"; 
+	if(!file_exists($f)){
+		// Init file
+		$fp=fopen("$f","w");
+		fputs($fp,"0");
+		fclose($fp);
+	}
+	// Update file : count + 1 
+	$fp=fopen("$f","r+");
+	$nb=fgets($fp,10);
+	$nb++;
+	fseek($fp,0);
+	fputs($fp,$nb);
+	fclose($fp);
+}
+
+function log_download($ver) {
+	$SEP = " | ";
+	$dh= date("Y-m-d H:i:s");
+	$row = $dh . $SEP;
+	if ( isset($_SERVER['REMOTE_ADDR']) ) {
+		$row = $row . $_SERVER['REMOTE_ADDR'];
+	}
+	$row = $row . $SEP;
+	if ( isset($_SERVER['HTTP_USER_AGENT']) ) {
+		$row = $row . $_SERVER['HTTP_USER_AGENT'];
+	}
+	$row = $row . $SEP . $ver;
+
+	$ym = date("Y-m");
+	$logfile = "log-" . $ym . ".txt";
+	$f = fopen($logfile, "a") or die("Unable to open log file!");
+	fwrite($f, $row . PHP_EOL );
+	fclose($f);
+}
+
+$fileFound = false ;
+if (isset($_POST['ver'])) {
+    //The parameter you need is present
+	$version = $_POST['ver'];
+	$filename = "telosys-cli-" . $version . ".zip";
+	if ( file_exists($filename) ) {
+		$fileFound = true ;
+		increment();
+		log_download($version);
+		header("Location:$filename");
+	}
+}
+if ( ! $fileFound ) {
+	header("HTTP/1.1 404 Not Found");
+	echo "<h1>File not found</h1>";
+}
+?>
